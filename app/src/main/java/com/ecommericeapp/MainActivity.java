3f6be8ce9f5e3f29fd2749835c009dfc620207a2
Jatinkataria123 <katariajatin99@gmail.com>
2024-02-28 23:27:49 +0530

    HELLO

--- app/src/main/java/com/ecommericeapp/MainActivity.java
+++ app/src/main/java/com/ecommericeapp/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
 System.out.println("ok sir ");
 
-        System.out.println(" hhhgh  hello  ");
+        System.out.println(" hhgh  hello  ");
     }
 }

3ae2422b887d64a204963a61a10bf29e695d8e65
tirth006 <156910853+tirth006@users.noreply.github.com>
2024-02-29 22:10:47 +0530

    remove

--- app/src/main/java/com/ecommericeapp/MainActivity.java
+++ app/src/main/java/com/ecommericeapp/MainActivity.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-            System.out.println("commiting and pushing");
-            System.out.println("");
+
 
 
 

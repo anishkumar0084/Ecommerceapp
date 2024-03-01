87abc9084e0031dafd0d257186d17afc06491242
Anish kumar <anishsinghjamsar@gmail.com>
2024-03-01 20:53:48 +0530

    new change4

---
+++ app/src/main/java/com/ecommericeapp/Reg_page.java
@@ -0,0 +1,15 @@
+package com.ecommericeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reg_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_page);
+
+    }
+}
